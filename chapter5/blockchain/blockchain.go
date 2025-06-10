4ba5c0507b6b99914e214e5316fb21b955e4ee5d
Wang Jun Rui <a0970606353@icloud.com>
2025-06-10 13:44:42 +0800

    add wallet module

---
+++ chapter5/blockchain/blockchain.go
@@ -0,0 +1,220 @@
+package blockchain
+
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/dgraph-io/badger"
+)
+
+const (
+	dbpath      = "./tmp/blocks"
+	dbfile      = "./tmp/blocks/MANIFEST"
+	genesisData = "First Transaction from Genesis"
+)
+
+type BlockChain struct {
+	LashHash []byte
+	Database *badger.DB
+}
+
+type BlockChainIterator struct {
+	CurrentHash []byte
+	Database    *badger.DB
+}
+
+func DBexists() bool {
+	if _, err := os.Stat(dbfile); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+func ContinueBlockChain(address string) *BlockChain {
+	if DBexists() == false {
+		fmt.Println("No existing blockchain found, create one!")
+		runtime.Goexit()
+	}
+
+	var lastHash []byte
+	opts := badger.DefaultOptions(dbpath)
+	db, err := badger.Open(opts)
+	Handle(err)
+	err = db.Update(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		item.Value(func(val []byte) error {
+			lastHash = val
+			return nil
+		})
+		return err
+	})
+	Handle(err)
+	chain := BlockChain{lastHash, db}
+	return &chain
+}
+
+func InitBlockChain(address string) *BlockChain {
+
+	if DBexists() {
+		fmt.Println("Blockchain already exist")
+		runtime.Goexit()
+	}
+	var lastHash []byte
+	opts := badger.DefaultOptions(dbpath)
+	db, err := badger.Open(opts)
+
+	err = db.Update(func(txn *badger.Txn) error {
+		cbtx := CoinbaseTx(address, genesisData)
+		genesis := Genesis(cbtx)
+		fmt.Println("Genesis Created")
+		err = txn.Set(genesis.Hash, genesis.Serialize())
+		Handle(err)
+		err = txn.Set([]byte("lh"), genesis.Hash)
+
+		lastHash = genesis.Hash
+		return err
+	})
+	Handle(err)
+
+	blockChain := BlockChain{lastHash, db}
+	return &blockChain
+
+}
+
+func (chain *BlockChain) AddBlock(transactions []*Transaction) {
+	var lastHash []byte
+
+	err := chain.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get([]byte("lh"))
+		item.Value(func(val []byte) error {
+			lastHash = val
+			return nil
+		})
+		return err
+	})
+	Handle(err)
+	newBlock := CreateBlock(transactions, lastHash)
+	err = chain.Database.Update(func(txn *badger.Txn) error {
+		err := txn.Set(newBlock.Hash, newBlock.Serialize())
+		Handle(err)
+		err = txn.Set([]byte("lh"), newBlock.Hash)
+
+		chain.LashHash = newBlock.Hash
+		return err
+	})
+	Handle(err)
+}
+
+func (chain *BlockChain) Iterator() *BlockChainIterator {
+	iter := &BlockChainIterator{chain.LashHash, chain.Database}
+	return iter
+}
+
+func (iter *BlockChainIterator) Next() *Block {
+	var block *Block
+
+	err := iter.Database.View(func(txn *badger.Txn) error {
+		item, err := txn.Get(iter.CurrentHash)
+		item.Value(func(val []byte) error {
+			block = Deserialize(val)
+			return nil
+		})
+		return err
+	})
+	Handle(err)
+
+	iter.CurrentHash = block.PreHash
+	return block
+}
+
+/*
+找出所有還包含(未被該地址花費)Output的交易
+*/
+func (chain *BlockChain) FindUnspentTransactions(address string) []Transaction {
+	var unspentTxs []Transaction
+
+	spentTXOs := make(map[string][]int) // KEY: 交易ID VALUE: 輸出的INDEX (記錄「哪些交易的哪些 output index 已經被花掉」)
+	iter := chain.Iterator()
+
+	for {
+		block := iter.Next()
+
+		for _, tx := range block.Transactions {
+			txID := hex.EncodeToString(tx.ID)
+			// 遍歷該交易的Outputs 檢查是否花掉
+		Outputs:
+			for outIdx, out := range tx.Outputs {
+				if spentTXOs[txID] != nil {
+					for _, spentOut := range spentTXOs[txID] {
+						if spentOut == outIdx {
+							continue Outputs
+						}
+					}
+				}
+
+				if out.CanBeUnlocked(address) {
+					unspentTxs = append(unspentTxs, *tx)
+				}
+			}
+
+			if !tx.IsCoinbase() {
+				for _, in := range tx.Inputs {
+					if in.CanUnlock(address) {
+						inTxID := hex.EncodeToString(in.ID)
+						spentTXOs[inTxID] = append(spentTXOs[inTxID], in.Out)
+					}
+				}
+			}
+		}
+
+		if len(block.PreHash) == 0 {
+			break
+		}
+	}
+	return unspentTxs
+}
+
+func (chain *BlockChain) FindUTXO(address string) []TxOutput {
+	var UTXOs []TxOutput
+
+	unspentTransactions := chain.FindUnspentTransactions(address)
+
+	for _, tx := range unspentTransactions {
+		for _, out := range tx.Outputs {
+			if out.CanBeUnlocked(address) {
+				UTXOs = append(UTXOs, out)
+			}
+		}
+	}
+
+	return UTXOs
+}
+
+/*
+從某個地址的UTXO中挑選足夠的輸出，來支付一筆交易
+*/
+func (chain *BlockChain) FindSpendableOutputs(address string, amount int) (int, map[string][]int) {
+	unspentOut := make(map[string][]int) // KEY: 交易ID VALUE: 輸出的INDEX (紀錄哪些輸出被選中來付款)
+	unspentTxs := chain.FindUnspentTransactions(address)
+	accumulated := 0
+
+Work:
+	for _, tx := range unspentTxs {
+		txID := hex.EncodeToString(tx.ID)
+
+		for outIdx, out := range tx.Outputs {
+			if out.CanBeUnlocked(address) && accumulated < amount {
+				accumulated += out.Value
+				unspentOut[txID] = append(unspentOut[txID], outIdx)
+
+				if accumulated > amount {
+					break Work
+				}
+			}
+		}
+	}
+
+	return accumulated, unspentOut
+}
